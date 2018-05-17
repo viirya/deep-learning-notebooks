eb3ffae48c03d837630f3376b335c3fb36f0a1bb
Liang-Chi Hsieh <liangchi.hsieh@linecorp.com>
2018-05-17 15:05:15 +0900

    Only actually loading images and annotations when loading batch.

--- FCN/BatchDatsetReader.py
+++ FCN/BatchDatsetReader.py
@@ -28,7 +28,6 @@ class BatchDatset:
         print(image_options)
         self.files = records_list
         self.image_options = image_options
-        self._read_images()
 
     def _read_images(self):
         self.__channels = True
@@ -36,8 +35,22 @@ class BatchDatset:
         self.__channels = False
         self.annotations = np.array(
            [np.expand_dims(self._transform(filename['annotation']), axis=3) for filename in self.files])
-        print (self.images.shape)
-        print (self.annotations.shape)
+
+    def _read_image_and_annotation(self, start, end):
+        """Read image files and annotation files content for the range of file records.
+        :param start: beginning offset in files.
+        :param end: ending offset in file.
+        """
+        files = self.files[start:end]
+
+        self.__channels = True
+        images = np.array([self._transform(filename['image']) for filename in files])
+        self.__channels = False
+        annotations = np.array(
+           [np.expand_dims(self._transform(filename['annotation']), axis=3) for filename in files])
+
+        return images, annotations
+         
 
     def _transform(self, filename):
         image = misc.imread(filename)
@@ -62,22 +75,24 @@ class BatchDatset:
     def next_batch(self, batch_size):
         start = self.batch_offset
         self.batch_offset += batch_size
-        if self.batch_offset > self.images.shape[0]:
+        if self.batch_offset > len(self.files):
             # Finished epoch
             self.epochs_completed += 1
             print("****************** Epochs completed: " + str(self.epochs_completed) + "******************")
             # Shuffle the data
-            perm = np.arange(self.files.shape[0])
+            perm = np.arange(len(self.files))
             np.random.shuffle(perm)
-            self.images = self.images[perm]
-            self.annotations = self.annotations[perm]
+            self.files = self.files[perm]
             # Start next epoch
             start = 0
             self.batch_offset = batch_size
 
         end = self.batch_offset
 
-        return self.images[start:end], self.annotations[start:end]
+        # Only load required images and annotations to save memory usage.
+        batch_images, batch_annotations = self._read_image_and_annotation(start, end)
+
+        return batch_images, batch_annotations
 
     def get_random_batch(self, batch_size):
         indexes = np.random.randint(0, self.images.shape[0], size=[batch_size]).tolist()
